598ecbf708967f111e020847a72620d85f23806c
iPizza <ianfigueroa507@gmail.com>
2022-05-24 09:26:41 -0300

    js

---
+++ clase5/index.js
@@ -0,0 +1,17 @@
+alert("Hola desde index.js");
+
+var primer = 12;
+console.log("nuevo msj");
+var segundo = primer + "a";
+let primer;
+const primero = 5;
+
+{
+  var mascota = "perro";
+  console.log(mascota);
+}
+
+{
+  var mascotas = ["perro", "gato"];
+  console.log(mascotas[1]);
+}
